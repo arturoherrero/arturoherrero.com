a1f33d77ab03ea8345b8e0b4805535e98f4b2de6
Arturo Herrero <arturo.herrero@gmail.com>
2025-04-21 21:11:16 +0200

    Updte exclamations to periods

--- _includes/greetings.js
+++ _includes/greetings.js
@@ -3,15 +3,15 @@
   let hour = new Date().getHours()
 
   if (hour <= 4)
-    document.write("Don't stay up too late!")
+    document.write("Don't stay up too late.")
   else if (hour <= 12)
-    document.write("Have a great day!")
+    document.write("Have a great day.")
   else if (hour <= 17)
-    document.write("Have a good afternoon!")
+    document.write("Have a good afternoon.")
   else if (hour <= 22)
-    document.write("Have a good evening!")
+    document.write("Have a good evening.")
   else if (hour <= 24)
-    document.write("Good night!")
+    document.write("Good night.")
 </script>
 
 <noscript>
